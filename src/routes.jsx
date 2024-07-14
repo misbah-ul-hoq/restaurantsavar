20afe92fd5eca8d7adf556199272fd8cfe8a6372
Misbah Ul Hoq <extraordinarymisbah@gmail.com>
2024-07-14 22:59:30 +0600

    created menu page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "./pages/Layout/Main";
 import Home from "./pages/Home";
+import MenuPage from "./pages/Menu";
 
 const routes = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const routes = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <MenuPage />,
       },
     ],
   },
